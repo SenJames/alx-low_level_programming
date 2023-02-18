996c4eba9c7fe970c77bec75a9bb1a990a3981bb
SenJames <ujames41@gmail.com>
2023-02-18 05:19:46 -0800

    ret

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Works and Returns 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcocme is a piece of art, ");
+	return (0);
+}
