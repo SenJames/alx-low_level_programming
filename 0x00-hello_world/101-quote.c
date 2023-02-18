b6717283c40bae9350eb2f30b01e8ba6cd832ac4
SenJames <ujames41@gmail.com>
2023-02-18 10:52:39 -0800

    ere

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\"
-			-Dora Korpar, 2015-10-19\n", stdout);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fputs(str, stdout);
 	return (1);
 }
