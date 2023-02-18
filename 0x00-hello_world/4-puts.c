0c4203dd6a50b156260b0dd1d46470be130e05c6
SenJames <ujames41@gmail.com>
2023-02-18 04:04:03 -0800

    ret

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+   char res[] = "Programming is like building a multilingual puzzle";
+   puts(res);
+
+   return 0;
+}
