3416788b71a0a34f039c39db6228bf2ad58ff9c0
SenJames <ujames41@gmail.com>
2023-02-24 15:59:24 -0800

    qweq

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * mul - Works for multiplying
+ * @a: check fot int
+ * @b: check for int
+ *
+ * Description: Maybe works
+ * Return: 0 (failure) or 1 (success)
+ */
+int mul(int a, int b)
+{
+	int sol = a * b;
+
+	return (sol);
+}
