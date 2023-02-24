55e2d2492dd42c1b34efb09618a1ef9ea0a904f0
SenJames <ujames41@gmail.com>
2023-02-24 10:34:28 -0800

    ret

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * main - Entry point
  * _isupper - A function that works
  * @c: Must be an integer
  *
@@ -19,15 +18,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
-}
-
-int main(void)
-{
-	char c;
-
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
 }
