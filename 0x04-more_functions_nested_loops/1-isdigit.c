3e842a46348c552949620362f89afd81b322d1f7
SenJames <ujames41@gmail.com>
2023-02-24 12:57:32 -0800

    34" "

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _isdigit - Works for checking upper
+ * @c: check fot int
+ *
+ * Description: Maybe works
+ * Return: 0 (failure) or 1 (success)
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
