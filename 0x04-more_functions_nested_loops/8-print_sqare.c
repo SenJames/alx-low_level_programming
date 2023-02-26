49b500b30bae8eb83b51a50d5037f4cbbc8dea51
SenJames <ujames41@gmail.com>
2023-02-26 08:57:46 -0800

    re

---
+++ 0x04-more_functions_nested_loops/8-print_sqare.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - This prints a line
+ * @size : The integer it takes in for printing
+ *
+ * Description : This function prints n number of lines
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int j, i;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
