d62cd2f2bfc71222b04eb837e9732f9ce6f9e639
SenJames <ujames41@gmail.com>
2023-02-26 07:42:30 -0800

    ree

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - This prints a line
+ * @n : The integer it takes in for printing
+ *
+ * Description : This function prints n number of lines
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
