83e8a4f64a6b61c3eefd6db7f0a8ad7cffb90aba
SenJames <ujames41@gmail.com>
2023-02-26 06:24:36 -0800

    re

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - Works for checking upper
+ *
+ * Description: Maybe works
+ */
+void more_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 14; c++)
+	{
+		_putchar((c / 10) + '0');
+		_putchar((c % 10) + '0');
+	}
+	_putchar('\n');
+}
