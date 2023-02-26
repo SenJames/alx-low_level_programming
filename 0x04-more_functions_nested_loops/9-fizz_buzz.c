0023c17c4b748444915ce15827900a017ecbab58
SenJames <ujames41@gmail.com>
2023-02-26 09:48:35 -0800

    eer

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * fizzbuzz - Works for fizzy drinks
+ *
+ * Description: Maybe works
+ * Return: 0 (failure) or 1 (success)
+ */
+void fizzbuzz(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+		if ((c % 3 == 0) && (c % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (c % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", c);
+		}
+		printf(" ");
+	}
+	printf("\n");
+}
+
+/**
+ * main - Entry Point
+ * Description: WOrks
+ * Return: 0
+ */
+
+int main(void)
+{
+	fizzbuzz();
+	return (0);
+}
