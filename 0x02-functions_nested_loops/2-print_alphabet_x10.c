f94cc83b31c5baa5d3e112f9f8d66f13ad2486bf
SenJames <ujames41@gmail.com>
2023-02-27 22:00:02 -0800

    ewew

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Works for checking upper
+ *
+ * Description: Maybe works
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
