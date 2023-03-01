4d68b3a68d0d04dd23ea12a7ba28c26aece1bb42
SenJames <ujames41@gmail.com>
2023-02-28 20:33:39 -0800

    ewew

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - Works for checking upper
+ * @c: check fot int
+ *
+ * Description: Maybe works
+ * Return: 0 (failure) or 1 (success)
+ */
+int _abs(int c)
+{
+	int val;
+
+	val = abs(c);
+	return (val);
+}
