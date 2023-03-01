bc34c5c945f4ec857355f4f6809a21b3c94a46c6
SenJames <ujames41@gmail.com>
2023-02-28 20:52:34 -0800

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - Works for checking upper
+ * @c: check fot int
+ *
+ * Description: Maybe works
+ * Return: 0 (failure) or 1 (success)
+ */
+int print_last_digit(int c)
+{
+	int val;
+
+	val = c % 10;
+	return (val);
+}
