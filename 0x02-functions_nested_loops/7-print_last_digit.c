631c17d331d594489088933ed328eab1deade4c3
SenJames <ujames41@gmail.com>
2023-02-28 21:21:55 -0800

    rewr

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,15 @@
  * @c: check fot int
  *
  * Description: Maybe works
+ * Return: 1(success)
  */
 int print_last_digit(int c)
 {
 	int val;
 
 	val = c % 10;
-	printf("%d", abs(val));
+	if (val < 0)
+		val = val * -1;
+	_putchar(val + '0');
+	return (val);
 }
