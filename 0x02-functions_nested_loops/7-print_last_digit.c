41efa8adc969d8eb2c652f0065b6e1f09e65ec4f
SenJames <ujames41@gmail.com>
2023-02-28 21:09:05 -0800

    eww

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,11 @@
  * @c: check fot int
  *
  * Description: Maybe works
- * Return: 0 (failure) or 1 (success)
  */
 int print_last_digit(int c)
 {
 	int val;
 
 	val = c % 10;
-	return (val);
+	printf("%d", abs(val));
 }
